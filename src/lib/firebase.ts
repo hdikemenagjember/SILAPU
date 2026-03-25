a9b39604ecbd673eb065f22e2a3ebd6e1f447acd
hdikemenagjember <hdikemenagjember@gmail.com>
2026-03-25 20:32:28 +0700

    feat: Force Google account selection on login
    This change updates the Google authentication provider to prompt the user to select an account each time they log in. This improves the user experience by ensuring they are using the intended account, especially when multiple Google accounts are signed in.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,6 +8,9 @@ export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
 export const auth = getAuth(app);
 
 export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+  prompt: 'select_account'
+});
 
 export const loginWithGoogle = async () => {
   try {
